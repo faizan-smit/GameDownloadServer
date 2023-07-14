c255f7ad5febf7f0f5045fad98e3ead7b6aae173
Muhammd Faizan Alam <121306012+faizan-smit@users.noreply.github.com>
2023-07-14 16:50:09 +0500

    MamaMia

---
+++ server.mjs
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+const __dirname = path.resolve();
+
+const app = express();
+
+
+app.use("/static", express.static(path.join(__dirname, 'static')))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Example server listening on port ${PORT}`)
+})
+
+
